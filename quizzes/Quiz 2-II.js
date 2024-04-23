02bdbaba92be8e36aaa04a83e0db487712650716
Carrolfaizer <147717752+Carrolfaizer@users.noreply.github.com>
2024-04-23 16:57:01 +0300

    Create Quiz 2-II.js

---
+++ quizzes/Quiz 2-II.js
@@ -0,0 +1,36 @@
+class ListNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    if (!head || !head.next) {
+        return null;
+    }
+
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow === fast) {
+            // Cycle detected
+            slow = head;
+            while (slow !== fast) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return slow; // Return the start of the cycle
+        }
+    }
+
+    return null; // No cycle
+};
